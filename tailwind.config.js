1102909941a916034bf549870413e268d3adc2d3
zaki bousmaha <zakib9@gmail.com>
2024-08-20 16:56:08 +0100

    theming & data
    add colors & JSON data to the project from the challenge design folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary": "#5358b6",
+        "secondary": "#f5f6fa",
+        "delete": "#ec6664"
+      },
+    },
   },
   plugins: [],
 }
